e58281f5255de70c4e309cd1c676d7b7e1d6c6e1
Vanilton Pinheiro <vanilton.pinheiro@fpf.br>
2022-01-07 22:45:27 -0400

    Incluindo cenario

--- src/test/java/bdd/glue/StepDefinitions.java
+++ src/test/java/bdd/glue/StepDefinitions.java
@@ -1,11 +1,59 @@
 package bdd.glue;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Map;
 
 public class StepDefinitions {
 
+    WebDriver driver;
+
     @Given("Estou no navegador Chrome")
     public void estou_no_navegador_chrome() {
-        System.out.println("Vamos começar a abrir o navegador por aqui baby");
+        driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
     }
+
+    @Given("Estou na url {string}")
+    public void estou_na_url(String string) {
+        driver.get(string);
+    }
+
+    @When("Preencho o vertice {string} com {string}")
+    public void preencho_o_vertice_com(String vertice, String valor) {
+        driver.findElement(By.name("V"+vertice)).sendKeys(valor);
+    }
+
+    @When("Preencho triângulo")
+    public void preencho_triângulo(io.cucumber.datatable.DataTable dataTable) {
+        for(Map<String, String> linha : dataTable.asMaps()){
+            String vertice = linha.get("vertice");
+            String valor = linha.get("valor");
+            driver.findElement(By.name("V"+vertice)).sendKeys(valor);
+        }
+    }
+
+    @When("Clico no botão {string}")
+    public void clico_no_botão(String nomeBotao) {
+        driver.findElement(By.xpath("//input[@value='"+nomeBotao+"']")).click();
+    }
+    @Then("É Exibido o triângulo {string}")
+    public void é_exibido_o_triângulo(String tipoTriangulo) {
+        Assert.assertTrue(driver.findElement(
+                By.xpath("//div[contains(.,'"+tipoTriangulo+"')]")).isDisplayed());
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+
 }
--- src/test/resources/features/triangulo.feature
+++ src/test/resources/features/triangulo.feature
@@ -2,3 +2,20 @@ Feature: Como um PROFESSOR eu gostaria de IDENTIFICAR OS TIPOS DE TRIÂNGULOS pa
 
   Scenario: Visualizar informações do triangulo Equilátero
     Given Estou no navegador Chrome
+    And Estou na url "http://vanilton.net/triangulo"
+    When Preencho o vertice "1" com "5"
+    And Preencho o vertice "2" com "5"
+    And Preencho o vertice "3" com "5"
+    And Clico no botão "Identificar"
+    Then É Exibido o triângulo "Equilátero"
+
+  Scenario: Visualizar informacoes do triangulo
+    Given Estou no navegador Chrome
+    And Estou na url "http://vanilton.net/triangulo"
+    When Preencho triângulo
+      |vertice|valor |
+      |1      |3     |
+      |2      |3     |
+      |3      |3     |
+    And Clico no botão "Identificar"
+    Then É Exibido o triângulo "Equilátero"
